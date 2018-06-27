401b613c432a1929eb927b0ee5f7d632d4bcb6f6
highwaycat <szabo.zsolt.gyorgy@gmail.com>
2018-06-27 21:52:21 +0200

    Spring Boot init
    Initializing Spring Boot, with new .gitignore.

---
+++ src/main/java/com/trantor/delugeadapter/DelugeAdapterApplication.java
@@ -0,0 +1,12 @@
+package com.trantor.delugeadapter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DelugeAdapterApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DelugeAdapterApplication.class, args);
+    }
+}
